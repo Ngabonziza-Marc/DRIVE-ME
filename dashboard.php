3278a654e21a1715d523ee05f92d7b75194189f0
@ngabonziza-marc <mkngabonziza@gmail.com>
2023-11-13 15:50:46 +0200

    new

--- dashboard.php
+++ dashboard.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/2a421485f4.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Dashboard</title>
 </head>
 <body>
-    <h1>
-        hello guys fuck you.
-    </h1>
+
 </body>
 </html>
